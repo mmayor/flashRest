fdba4f7514a0f8f4528e1751f3ae043a5001a0eb
mmayor <mmayor200610@gmail.com>
2019-09-10 02:11:40 +0000

    “Update”

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import Recetas, Contacts
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,14 +28,159 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
+@app.route('/contact', methods=['GET'])
 def handle_person():
 
     response_body = {
-        "hello": "world"
+        "ingredients": "tomatte, fish"
     }
 
     return jsonify(response_body), 200
+
+@app.route('/recetas', methods=['GET'])
+def handle_recetas():
+
+    recetasTemp = Recetas()
+
+
+    ## UPDATE DATA
+
+
+    receta2 = Recetas.query.get(5)
+    if receta2 is None:
+        raise APIException('User not found', status_code=404)
+
+    ##if "name" in body:
+    receta2.name = 'newName'
+    ##if "label" in body:
+    receta2.label = 'newLabel'
+    db.session.commit()
+
+
+    ## DELETE DATA
+    '''
+    receta3 = Recetas.query.get(6)
+    if receta3 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(receta3)
+    db.session.commit()
+    '''
+    ## Inserting data
+
+##    Asuming you have a Person object in your models.py file.
+
+    '''
+    receta1 = Recetas(name="colPaella", label="colPaella@email.com")
+    db.session.add(receta1)
+    db.session.commit()
+    '''
+
+
+    # get all the people
+    recetas_query = recetasTemp.query.all()
+    # get only the ones named "Joe"
+    receta_query = recetasTemp.query.filter_by(name='pollo')
+    # map the results and your list of people its now inside all_people variable
+    all_recetas = list(map(lambda x: x.serialize(), recetas_query))
+
+    return jsonify(all_recetas), 200
+
+##CONTACTS
+
+
+#DELETE A CONTACT
+@app.route('/delete_contact/<int:contact_id>', methods=['DELETE'])
+def delete_person(contact_id):
+    user1 = Contacts.query.get(contact_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    return "ok", 200
+
+
+##UPDATE Contacts
+
+#UPDATE A CONTACT
+@app.route('/update_contact/<int:contact_id>', methods=['PUT'])
+def update_contact(contact_id):
+    body = request.get_json(contact_id)
+    user1 = Contacts.query.get(contact_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    if "full_name" in body:
+        user1.full_name = body["full_name"]
+    if "email" in body:
+        user1.email = body["email"]
+    if "agenda_slug" in body:
+        user1.agenda_slug = body["agenda_slug"]
+    if "address" in body:
+        user1.address = body["address"]
+    if "phone" in body:
+        user1.phone = body["phone"]
+    db.session.commit()
+    return jsonify(user1.serializeContact()), 200
+
+##ADD Contacts
+
+#ADD A CONTACT
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    body = request.get_json()
+    user1 = Contacts(full_name=body["full_name"], email=body["email"], agenda_slug=body["agenda_slug"], address=body["address"], phone=body["phone"])
+    db.session.add(user1)
+    db.session.commit()
+    return "ok", 200
+
+## GET CONTACT
+@app.route('/contactsNew', methods=['GET'])
+def handle_contacts():
+
+        contactsTemp = Contacts()
+
+
+        ## UPDATE DATA
+
+        '''
+        receta2 = Recetas.query.get(5)
+        if receta2 is None:
+            raise APIException('User not found', status_code=404)
+
+        ##if "name" in body:
+        receta2.name = 'newName'
+        ##if "label" in body:
+        receta2.label = 'newLabel'
+        db.session.commit()
+            '''
+
+        ## DELETE DATA
+        '''
+        receta3 = Recetas.query.get(6)
+        if receta3 is None:
+            raise APIException('User not found', status_code=404)
+        db.session.delete(receta3)
+        db.session.commit()
+        '''
+        ## Inserting data
+
+    ##    Asuming you have a Person object in your models.py file.
+
+        '''
+        receta1 = Recetas(name="colPaella", label="colPaella@email.com")
+        db.session.add(receta1)
+        db.session.commit()
+        '''
+
+
+        # get all the people
+        contacts_query = contactsTemp.query.all()
+        # get only the ones named "Joe"
+        # receta_query = recetasTemp.query.filter_by(name='pollo')
+        # map the results and your list of people its now inside all_people variable
+        all_contacts = list(map(lambda x: x.serializeContact(), contacts_query))
+
+        return jsonify(all_contacts), 200
+
 
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Recetas(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(80), unique=True, nullable=False)
+     label = db.Column(db.String(120), unique=True, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+     def __repr__(self):
+         return '<Recetas %r>' % self.name
+
+     def serialize(self):
+         return {
+             "name": self.name,
+             "label": self.label
+         }
+
+class Contacts(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        full_name= db.Column(db.String(80), unique=True, nullable=False)
+        email= db.Column(db.String(80), unique=True, nullable=False)
+        agenda_slug= db.Column(db.String(80), unique=False, nullable=False)
+        address= db.Column(db.String(80), unique=False, nullable=False)
+        phone= db.Column(db.String(80), unique=False, nullable=False)
+
+        def __repr__(self):
+            return '<Contacts %r>' % self.full_name
+
+        def serializeContact(self):
+            return {
+             "id": self.id,
+             "full_name": self.full_name,
+             "email": self.email,
+             "agenda_slug": self.agenda_slug,
+             "address": self.address,
+             "phone": self.phone
+         }
